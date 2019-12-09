82fe396ab008cb9e23c2f12a7ef06767420d5192
raja <sainiraja59@gmail.com>
2019-12-09 12:15:41 +0530

    form and http module added in both modules

--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -4,11 +4,14 @@ import { CustomerRoutingModule } from './customer.routing';
 import { CustomerComponent } from './customer/customer.component';
 import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule,ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [CustomerComponent, HomeComponent, DetailsComponent],
   imports: [
-    CommonModule,CustomerRoutingModule
+    CommonModule,CustomerRoutingModule,FormsModule,ReactiveFormsModule,
+    HttpClientModule
   ]
 })
 export class CustomerModule { }
--- src/app/jokes/jokes.module.ts
+++ src/app/jokes/jokes.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { ProgrammingComponent } from './programming/programming.component';
 import { DarkComponent } from './dark/dark.component';
 import { JokesComponent } from './jokes/jokes.component';
-import { JokesRoutingModule} from './jokes.routing'
+import { JokesRoutingModule} from './jokes.routing';
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule,ReactiveFormsModule } from "@angular/forms";
 @NgModule({
   declarations: [ProgrammingComponent, DarkComponent, JokesComponent],
   imports: [
     CommonModule,
-    JokesRoutingModule
-
+    JokesRoutingModule,FormsModule,ReactiveFormsModule,
+    HttpClientModule
   ]
 })
 export class JokesModule { }
